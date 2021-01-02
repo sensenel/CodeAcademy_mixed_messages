b24cce3412965bdd88c15546eec026b8823084e9
Chris Gabler <ChriGa-MacPro@chriss-mac-pro.speedport_w_921v_1_46_000>
2021-01-02 17:32:41 +0100

    new version v1

--- script.js
+++ script.js
@@ -1,36 +1,16 @@
 "use strict";
 
 const cooKieOutput = {
-    savedSpells: [],
-    possibleSpells: ["Nothing is as easy as it looks.",
-     "Everything takes longer than you think.", "Anything that can go wrong will go wrong.",
-     "If there is a possibility of several things going wrong, the one that will cause the most damage will be the one to go wrong.",
-     "If there is a worse time for something to go wrong, it will happen then.",
-     "If anything simply cannot go wrong, it will anyway.",
-     "If you perceive that there are four possible ways in which a procedure can go wrong, and circumvent these, then a fifth way, unprepared for, will promptly develop.",
-     "Left to themselves, things tend to go from bad to worse.",
-     "If everything seems to be going well, you have obviously overlooked something.",
-     "Nature always sides with the hidden flaw.",
-     "Mother nature is a bitch.",
-     "It is impossible to make anything foolproof because fools are so ingenious.",
-     "haha, just kidding"],
+    signs: ["Sun", "Moon", "Mars", "Andromeda", "Beteigeuze", "Butterfly", "Worm", "SuperNova", "giggling Star"],
+    fortunes: ["good luck", "bad times", "huge changes", "lost in faith", "nearby danger", "long lasting sexual pleasure"],
+    advice: ["trust no one", "trust live", "trust the person next to you", "run away!", "calm down", "let you go", "think about live", "have fun!"],
      pullSpell () {
-         let num = Math.floor(Math.random() * this.possibleSpells.length),
-         token = this.possibleSpells[num];
-         console.log("---- " + token +  " ----");
-         this.savedSpells.push(token);
-     },
-     delSavedSpells () {
-         this.savedSpells = [];
-         console.log(this.savedSpells);
-     },
-     outputSaved () {
-         this.savedSpells.forEach(spell => {
-            console.log(spell);
-         });
+        console.log('\n############## ------------- ############### \n');
+        console.log(`Your sign is ${this.signs[Math.floor(Math.random() * this.signs.length)]} \n`);
+        console.log(`You are having ${this.fortunes[Math.floor(Math.random() * this.fortunes.length)]} \n`);
+        console.log(`You should ${this.advice[Math.floor(Math.random() * this.advice.length)]} \n`);
+        console.log('############## ------------- ###############\n');
      }
 }
 
 cooKieOutput.pullSpell();
-cooKieOutput.outputSaved();
-//cooKieOutput.delSavedSpells();
