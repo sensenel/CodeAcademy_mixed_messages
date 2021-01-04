f103f5485fb2c6af2830648f24b6b23069bb2a9d
Chris Gabler <ChriGa-MacPro@chriss-mac-pro.speedport_w_921v_1_46_000>
2021-01-04 19:54:23 +0100

    complete new random Version 04012021

--- script.js
+++ script.js
@@ -1,16 +1,67 @@
 "use strict";
 
 const cooKieOutput = {
-    signs: ["Sun", "Moon", "Mars", "Andromeda", "Beteigeuze", "Butterfly", "Worm", "SuperNova", "giggling Star"],
-    fortunes: ["good luck", "bad times", "huge changes", "lost in faith", "nearby danger", "long lasting sexual pleasure"],
-    advice: ["trust no one", "trust live", "trust the person next to you", "run away!", "calm down", "let you go", "think about live", "have fun!"],
+    signs: [
+         { positiv: ["Sun", "Andromeda", "Butterfly"]
+         },
+         { negativ: ["Moon", "Mars", "SuperNova"]
+         },
+         { neutral:  ["Worm", "Beteigeuze", "giggling Star"]
+         }
+   ],
+    fortunes: [
+         { positiv: ["good luck", "happiness", "kingsize fortune"],
+         },
+         { negativ: ["bad times", "tremendous trouble", "nearby danger"]
+
+         },
+         { neutral: ["huge changes", "lost in faith", "long lasting sexual pleasure"] 
+         } 
+      ],
+    advices: [
+         { positiv: ["have fun!", "let you go", "hug someone!"]
+         },
+         { negativ: ["trust no one", "run away!", "hide!"]
+         },
+         { neutral: ["trust live", "trust the person next to you", "think about live"]
+         }
+      ],
      pullSpell () {
+        let dice = (arr) => {
+            return Math.floor(Math.random() * arr.length);
+        }
+        let signObjs = this.signs[dice(this.signs)],
+        signProp = [],
+        sign = '',
+        fortune = '',
+        advice = '';
+        for (let prop in signObjs) {
+           signProp = prop;
+           let signIndex = dice(signObjs[prop]); 
+           sign = signObjs[prop][signIndex];
+        }
+        switch(signProp) {
+           case "positiv" :   fortune = this.fortunes[0].positiv[dice(this.fortunes[0].positiv)]; 
+                              advice = this.advices[0].positiv[dice(this.advices[0].positiv)];
+           break;
+           case "negativ" :   fortune = this.fortunes[1].negativ[dice(this.fortunes[1].negativ)];
+                              advice = this.advices[1].negativ[dice(this.advices[1].negativ)];
+           break;
+           case "neutral" :   fortune = this.fortunes[2].neutral[dice(this.fortunes[2].neutral)]; 
+                              advice = this.advices[2].neutral[dice(this.advices[2].neutral)];
+           break;
+           default: fortune = 'error'; advice = 'error';
+           break;
+        }
+
         console.log('\n############## ------------- ############### \n');
-        console.log(`Your sign is ${this.signs[Math.floor(Math.random() * this.signs.length)]} \n`);
-        console.log(`You are having ${this.fortunes[Math.floor(Math.random() * this.fortunes.length)]} \n`);
-        console.log(`You should ${this.advice[Math.floor(Math.random() * this.advice.length)]} \n`);
+        console.log(`Your sign is ${sign} \n`);
+        console.log(`You are having ${fortune} \n`);
+        console.log(`You should ${advice} \n`);
         console.log('############## ------------- ###############\n');
      }
 }
 
 cooKieOutput.pullSpell();
+
+//console.log(cooKieOutput.signs[0].positiv);
